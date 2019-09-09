ba8043e2eb7b444789a424754935f991572c25a1
Jakub Rożek <jakub@stoplight.io>
2019-09-09 17:42:42 +0200

    Commit dist/

---
+++ dist/src/dumper.d.ts
@@ -0,0 +1,14 @@
+import { SchemaDefinition } from "./schema";
+export interface DumpOptions {
+    indent?: number;
+    noArrayIndent?: boolean;
+    skipInvalid?: boolean;
+    flowLevel?: number;
+    styles?: {
+        [x: string]: any;
+    };
+    schema?: SchemaDefinition;
+    lineWidth?: number;
+}
+export declare function dump(input: any, options?: DumpOptions): string;
+export declare function safeDump(input: any, options?: DumpOptions): string;
